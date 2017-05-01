7d8b3df4a7f5e9e0d353e4cf7c283581add7d02c
Hunter <hkurtis89@gmail.com>
2017-04-30 21:14:36 -0700

    Test case

--- README.md
+++ README.md
@@ -1,9 +1,41 @@
 ﻿# proj4
-Xinu Project 4 for OS 475
+Xinu Project 4 for OS 475 Evan, Mark, Hunter
 
-Hunter Kurtis – Finished part 3.
+Test case is in main_dl.c:
+In our test case we have 4 worker threads and 4 different locks. 
+Worker 0 requires lock 0 first, and lock 1 second. 
+Worker 1 requires lock 2 first, and lock 3 second. 
+Worker 2 requires lock 0 first, and lock 1 second. 
+Finally, Worker 3 requires lock 3 first, and  lock 2 second. 
 
-Mark - We need to figure out the backtracking and double check that the deadlock detection works it seems to
-but not sure. 
+What is expected to happen without Worker 3 is as follows: 
+Worker 0 acquires lock 0 and does work. 
+Worker 1 acquires lock 2 and does work. 
+Worker 1 acquires lock 3 and does work. 
+Worker 0 acquires lock 1 and does work. 
+Worker 1 releases lock 2. 
+Worker 1 releases lock 3. 
+Worker 0 releases lock 1. 
+Worker 0 releases lock 0.
+Worker 2 acquires lock 2 and does work.
+Worker 2 acquires lock 1 and does work.
+Worker 2 releases lock 1.
+Worker 2 releases lock 2.
+
+When we add worker 3 a deadlock occurs:
+Worker 0 acquires lock 0 and does work.
+Worker 1 acquires lock 2 and does work.
+Worker 3 acquires lock 3 and does work.
+Worker 0 acquires lock 1 and does work.
+Worker 0 releases lock 1.
+Worker 0 releases lock 0.
+Worker 2 acquires lock 0 and does work.
+Worker 2 acquires lock 1 and does work.
+Worker 2 releases lock 1.
+Worker 2 releases lock 0.
+Since Worker 3 is holding onto lock 2 and Worker 1 is holding onto lock 3, no work can be done for either process and a deadlock occurs.
+
+With deadlock detection, when Worker 3 grabs lock 3, the process is killed in order for process 1 to complete.
+
 
 
--- system/main_dl.c
+++ system/main_dl.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define N 2
+#define N 5 //originally 2
 
 lid32	printer_lock;
 lid32	mylock[N];
@@ -40,24 +40,95 @@ void	work(uint32 id)
  */
 void	worker(uint32 id)
 {
-	if (id == 0)
-	{
+	if(id == 0){
+		//acquire lock 0
 		acquire(mylock[0]);
+		kprintf("worker 0 has lock 0\n");
+		//print something
 		work(id);
+		//acquire lock 1
 		acquire(mylock[1]);
+		kprintf("worker 0 has lock 1\n");
+		//print something
 		work(id);
+		//release lock 1
+		kprintf("worker 0 is about to release lock 1\n");
 		release(mylock[1]);
+		//release lock 0
+		kprintf("worker 0 is about to release lock 0\n");
+		release(mylock[0]);
+	}else if(id == 1){
+		//acquire lock 2
+		acquire(mylock[2]);
+		kprintf("worker 1 has lock 2\n");
+		//print something
+		work(id);
+		//acquire lock 1
+		acquire(mylock[3]);
+		kprintf("worker 1 has lock 3\n");
+		//print something
+		work(id);
+		//release lock 1
+		kprintf("worker 1 is about to release lock 2\n");
+		release(mylock[2]);
+		//release lock 2
+		kprintf("worker 1 is about to release lock 3\n");
+		release(mylock[3]);
+	}else if(id==2){//id == 2
+		//acquire lock 2
+		acquire(mylock[0]);
+		kprintf("worker 2 has lock 0\n");
+		//print something
+		work(id);
+		//acquire lock 1
+		acquire(mylock[1]);
+		kprintf("worker 2 has lock 1\n");
+		//print something
+		work(id);
+		//release lock 1
+		kprintf("worker 2 is about to release lock 1\n");
+		release(mylock[1]);
+		//release lock 0
+		kprintf("worker 2 is about to release lock 0\n");
+		release(mylock[0]);
+	 }else{//id == 3
+		//acquire lock 3
+		acquire(mylock[3]);
+		kprintf("worker 3 has lock 3\n");
+		//print something
+		work(id);
+		//acquire lock 2
+		acquire(mylock[2]);
+		kprintf("worker 3 has lock 2\n");
+		//print something
+		work(id);
+		//release lock 2
+		kprintf("worker 3 is about to release lock 2\n");
+		release(mylock[2]);
+		//release lock 3
+		kprintf("worker 3 is about to release lock 3\n");
 		release(mylock[0]);
 	}
-	else
-	{
-		acquire(mylock[1]);
-		work(id);
-		acquire(mylock[0]);
-		work(id);
-		release(mylock[0]);
-		release(mylock[1]);
-	}
+
+	// }
+	// if (id == 0)
+	// {
+	// 	acquire(mylock[0]);
+	// 	work(id);
+	// 	acquire(mylock[1]);
+	// 	work(id);
+	// 	release(mylock[1]);
+	// 	release(mylock[0]);
+	// }
+	// else
+	// {
+	// 	acquire(mylock[1]);
+	// 	work(id);
+	// 	acquire(mylock[0]);
+	// 	work(id);
+	// 	release(mylock[0]);
+	// 	release(mylock[1]);
+	// }
 }
 
 int	main(uint32 argc, uint32 *argv)
@@ -69,6 +140,9 @@ int	main(uint32 argc, uint32 *argv)
 
 	ready(create((void*) worker, INITSTK, 15, "Worker 0", 1, 0), FALSE);
 	ready(create((void*) worker, INITSTK, 15, "Worker 1", 1, 1), FALSE);
+	ready(create((void*) worker, INITSTK, 15, "Worker 1", 1, 2), FALSE);
+	ready(create((void*) worker, INITSTK, 15, "Worker 1", 1, 3), FALSE);
+	ready(create((void*) worker, INITSTK, 15, "Worker 1", 1, 4), FALSE);
 
 	return 0;
 }
